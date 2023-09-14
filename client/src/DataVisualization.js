b3536dc51880800acbfbc978f1df04cb94545a46
Marsha <marshatiisa35@gmail.com>
2023-09-14 15:01:40 -0400

    add area chart

--- client/src/DataVisualization.js
+++ client/src/DataVisualization.js
@@ -1,10 +1,12 @@
 import DataLineChart from "./DataLineChart"
+import DataAreaChart from "./DataAreaChart"
 
 function DataVisualization({data}){
     console.log('DV', data)
     return (
         <div>
             <DataLineChart data={data}/>
+            <DataAreaChart data={data} />
         </div>
     )
 }
